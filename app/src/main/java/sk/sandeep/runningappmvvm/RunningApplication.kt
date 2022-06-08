231da7627b262e101ee879155939418ac2847e12
Sandeep <sandeepsk15892>
2022-06-08 14:48:12 +0530

    dagger hilt settings

---
+++ app/src/main/java/sk/sandeep/runningappmvvm/RunningApplication.kt
@@ -0,0 +1,7 @@
+package sk.sandeep.runningappmvvm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RunningApplication : Application()
---
+++ app/src/main/java/sk/sandeep/runningappmvvm/di/AppModule.kt
@@ -0,0 +1,40 @@
+package sk.sandeep.runningappmvvm.di
+
+import android.content.Context
+import androidx.room.Room
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import sk.sandeep.runningappmvvm.data.local.RunningDatabase
+import sk.sandeep.runningappmvvm.util.Constants.RUNNING_DATABASE_NAME
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(
+        @ApplicationContext app: Context
+    ) = Room.databaseBuilder(
+        app,
+        RunningDatabase::class.java,
+        RUNNING_DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+}
+
+
+
+
+
+
+
+
